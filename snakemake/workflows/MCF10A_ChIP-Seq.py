93b4a663b89cbd98af9e0f47e65b540ded9afb99
Sebastian Kurscheid <sebastian.kurscheid@anu.edu.au>
2017-08-23 20:15:24 +1000

    adds hopefully activated adapter removal

--- snakemake/workflows/MCF10A_ChIP-Seq.py
+++ snakemake/workflows/MCF10A_ChIP-Seq.py
@@ -18,17 +18,21 @@ include_prefix= os.environ['HOME'] + "/Development/JCSMR-Tremethick-Lab/Breast/s
 include:
     include_prefix + "run_kallisto.py"
 
-rule AdapterRemoval:
+rule run_AdapterRemoval:
     params:
+        threads = config["AdapterRemoval"]["threads"]
     input:
+        read1 = config["samples"][wildcards["assayID"]][wildcards["runID"]][wildcards["unit"]][0],
+        read2 = config["samples"][wildcards["assayID"]][wildcards["runID"]][wildcards["unit"]][1]
     output:
-    run:
+        read1 = "{assayID}/{runID}/{outdir}/{trim_dir}/{unit}_R1.fastq.gz",
+        read2 = "{assayID}/{runID}/{outdir}/{trim_dir}/{unit}_R2.fastq.gz"
+    wrapper:
+        "0.1.0/bio/AdapterRemoval"
 
-rule all:
+rule run_AdapterRemoval:
     input:
-        expand("{assayID}/{runID}/{outdir}/{trim_dir}/{unit}",
-               assayID = "ChIP-Seq",
-               runID = config["ChIP-Seq"]["runID"],
-               outdir = config["processed_dir"],
-               trim_dir = config["trim_dir"]
-               unit = config["samples"]["ChIP-Seq"][config["ChIP-Seq"]["runID"][0]])
+        for i in config["samples"]["ChIP-Seq"]["runID"]:\
+            for j in config["samples"]["ChIP-Seq"][i]:\
+                join("ChIP-Seq/" + i + "/" + config["processed_dir"] + "/" + config["trim_dir"] + "/" + j + "_R1.fastq.gz")\
+                join("ChIP-Seq/" + i + "/" + config["processed_dir"] + "/" + config["trim_dir"] + "/" + j + "_R2.fastq.gz")
