1d8eadf36fa4b98a25e6344e80d98f6b1e573b56
Sebastian Kurscheid <genomicsio@gmail.com>
2017-08-28 10:37:36 +1000

    adds second stage workflow

---
+++ snakemake/workflows/MCF10A_ChIP-Seq_deepTools_QC.py
@@ -0,0 +1,90 @@
+_author__ = "Sebastian Kurscheid (sebastian.kurscheid@anu.edu.au)"
+__license__ = "MIT"
+__date__ = "2017-08-23"
+
+from snakemake.exceptions import MissingInputException
+import os
+from os.path import join
+
+rule:
+    version: 0.1
+
+localrules:
+    all
+
+# variables
+home_dir = os.environ['HOME']
+home = home_dir
+
+wrapper_dir = os.environ['HOME'] + "/Development/snakemake-wrappers/bio"
+
+REF_GENOME = "hg19"
+REF_VERSION = config["references"][REF_GENOME]["version"][1]
+
+# includes
+include_prefix= os.environ['HOME'] + "/Development/JCSMR-Tremethick-Lab/Breast/snakemake/rules/"
+
+# input functions
+def getAllFASTQ(wildcards):
+    fn =[]
+    for i in config["samples"][wildcards["assayID"]][wildcards["runID"]]:
+        for j in config["samples"][wildcards["assayID"]][wildcards["runID"]][i]:
+            fn.append("/".join([wildcards["assayID"], wildcards["runID"], config["raw_dir"], j]))
+    return(fn)
+
+def getAllBAMs(duplicates):
+    fn = []
+    for i in config["samples"]["ChIP-Seq"]["runID"]:
+        for j in config["samples"]["ChIP-Seq"][i]:
+            fn.append(join("ChIP-Seq/" + i + "/" + config["processed_dir"] + "/" + REF_VERSION + "/bowtie2/" + duplicates + "/" + j + ".Q" + config["alignment_quality"] + ".sorted.bam"))
+    return(fn)
+
+def get_sample_labels(wildcards):
+    sl = []
+    runIDs = config["samples"][wildcards["assayID"]]["runID"]
+    for i in runIDs:
+        for k in config["samples"][wildcards["assayID"]][i].keys():
+            sl.append(k)
+    return(sl)
+
+
+# set targets here
+PROCESSED_BAMs = expand("{assayID}/{file}",
+                        assayID = "ChIP-Seq",
+                        file = [ i + "/" + config["processed_dir"] + "/" + REF_VERSION + "/bowtie2/duplicates_removed"  + "/" + j + ".Q" + config["alignment_quality"] + ".sorted.bam.bai" \
+                            for i in config["samples"]["ChIP-Seq"]["runID"] \
+                                for j in config["samples"]["ChIP-Seq"][i]])
+
+
+rule multiBamSummary:
+    version:
+        0.2
+    params:
+        deepTools_dir = home + config["deepTools_dir"],
+        binSize = config["program_parameters"]["deepTools"]["binSize"],
+        labels = get_sample_labels
+    threads:
+        24
+    input:
+        getAllBAMs("duplicates_marked")
+    output:
+        npz = "{assayID}/{outdir}/{reference_version}/deepTools/multiBamSummary/duplicates_marked/results.npz"
+    shell:
+        """
+            {params.deepTools_dir}/multiBamSummary bins --bamfiles {input} \
+                                                        --labels {params.labels} \
+                                                        --numberOfProcessors {threads} \
+                                                        --centerReads \
+                                                        --binSize {params.binSize} \
+                                                        --outFileName {output.npz}
+        """
+
+
+
+# target rules
+rule all:
+    input:
+        expand("{assayID}/{outdir}/{reference_version}/deepTools/multiBamSummary/duplicates_marked/results.npz",
+               assayID = "ChIP-Seq",
+               outdir = "processed_data",
+               reference_version = REF_VERSION)
