3a4318a6abfaa3888ed4a85ba429946ed48acb36
Sebastian Kurscheid <genomicsio@gmail.com>
2017-12-01 10:34:14 +1100

    minor changes

--- snakemake/workflows/MCF10A_RNA-Seq_STAR_only.py
+++ snakemake/workflows/MCF10A_RNA-Seq_STAR_only.py
@@ -7,12 +7,15 @@ from snakemake.exceptions import MissingInputException
 import os
 
 rule:
-    version: 0.4
+    version: 0.1
 
 localrules:
-    all, run_kallisto, run_STAR, run_htseq, run_cutadapt
+    all, run_STAR
 
-home = os.environ['HOME']
+# variables
+REF_GENOME="hg19"
+REF_VERSION="GRCh37_hg19_ensembl75"
+HOME=os.environ['HOME']
 
 wrapper_dir = os.environ['HOME'] + "/Development/snakemake-wrappers/bio"
 
@@ -21,77 +24,15 @@ include_prefix= os.environ['HOME'] + "/Development/JCSMR-Tremethick-Lab/HP1-alph
 assay_dir = "NB501086_0114_B_Azad_JCSMR_hRNAseq"
 
 include:
-     include_prefix + "perform_fastqc.py"
-include:
-    include_prefix + "perform_cutadapt.py"
-include:
-    include_prefix + "run_kallisto.py"
-include:
     include_prefix + "run_STAR.py"
 
-rule run_fastqc:
-    input:
-        "RNA-Seq/NB501086_0114_B_Azad_JCSMR_hRNAseq/processed_data/reports/"
-
-rule run_kallisto:
-    input:
-        expand("{assayID}/NB501086_0114_B_Azad_JCSMR_hRNAseq/{outdir}/{reference_version}/kallisto/{unit}",
-               assayID = "RNA-Seq",
-               outdir = config["processed_dir"],
-               #reference_version = config["references"]["hg38"]["version"],
-               reference_version = "GRCh38_RefSeq_NM",
-               unit = config["samples"]["RNA-Seq"]["NB501086_0114_B_Azad_JCSMR_hRNAseq"].keys()),
-        expand("{assayID}/NB501086_0082_RDomaschenz_JCSMR_mRNAseq/{outdir}/{reference_version}/kallisto/{unit}",
-               assayID = "RNA-Seq",
-               outdir = config["processed_dir"],
-               #reference_version = config["references"]["hg38"]["version"],
-               reference_version = "GRCh38_RefSeq_NM",
-               unit = config["samples"]["RNA-Seq"]["NB501086_0082_RDomaschenz_JCSMR_mRNAseq"].keys())
-
-rule run_STAR:
-    input:
-        expand("{assayID}/NB501086_0114_B_Azad_JCSMR_hRNAseq/{outdir}/{reference_version}/STAR/full/{unit}.aligned.bam",
-               assayID = "RNA-Seq",
-               outdir = config["processed_dir"],
-               reference_version = config["references"]["hg38"]["version"],
-               unit = config["samples"]["RNA-Seq"]["NB501086_0114_B_Azad_JCSMR_hRNAseq"].keys())
-
-rule run_STAR_untrimmed:
-    input:
-        expand("{assayID}/{runID}/{outdir}/{reference_version}/STAR/full/untrimmed/{unit}.aligned.bam",
-               assayID = "RNA-Seq",
-               runID = "NB501086_0114_B_Azad_JCSMR_hRNAseq",
-               outdir = config["processed_dir"],
-               reference_version = config["references"]["hg38"]["version"],
-               unit = config["samples"]["RNA-Seq"]["NB501086_0114_B_Azad_JCSMR_hRNAseq"].keys())
-
-rule run_cutadapt:
-    input:
-        expand("{assayID}/{runID}/{outdir}/{trim_data}/{unit}_{suffix}.QT.CA.fastq.gz",
-               assayID = "RNA-Seq",
-               runID = "NB501086_0114_B_Azad_JCSMR_hRNAseq",
-               outdir = config["processed_dir"],
-               trim_data = config["trim_dir"],
-               unit = config["samples"]["RNA-Seq"]["NB501086_0114_B_Azad_JCSMR_hRNAseq"].keys(),
-               suffix = ["R1_001", "R2_001"])
+# targets
+STARS = expand("{assayID}/{file}",
+              assayID = "RNA-Seq",
+              file = [ i + "/" + config["processed_dir"] + "/" + REF_VERSION + "/STAR/full/" + j + ".bam.bai"\
+                  for i in config["samples"]["RNA-Seq"]["runID"] \
+                      for j in config["samples"]["RNA-Seq"][i]])
 
 rule all:
     input:
-        expand("{assayID}/{runID}/{outdir}/{reference_version}/kallisto/{unit}",
-               assayID = "RNA-Seq",
-               runID = "NB501086_0114_B_Azad_JCSMR_hRNAseq",
-               outdir = config["processed_dir"],
-               reference_version = config["references"]["hg38"]["version"],
-               unit = config["samples"]["RNA-Seq"]["NB501086_0114_B_Azad_JCSMR_hRNAseq"].keys()),
-        expand("{assayID}/{runID}/{outdir}/{reference_version}/kallisto/{unit}",
-               assayID = "RNA-Seq",
-               runID = "NB501086_0082_RDomaschenz_JCSMR_mRNAseq",
-               outdir = config["processed_dir"],
-               reference_version = config["references"]["hg38"]["version"],
-               unit = config["samples"]["RNA-Seq"]["NB501086_0082_RDomaschenz_JCSMR_mRNAseq"].keys()),
-        expand("{assayID}/{runID}/{outdir}/{reference_version}/STAR/full/{unit}.aligned.bam",
-               assayID = "RNA-Seq",
-               runID = "NB501086_0114_B_Azad_JCSMR_hRNAseq",
-               outdir = config["processed_dir"],
-               reference_version = config["references"]["hg38"]["version"],
-               unit = config["samples"]["RNA-Seq"]["NB501086_0114_B_Azad_JCSMR_hRNAseq"].keys()),
+        STARS
